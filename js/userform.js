0286b8e22661c3f4526a510901527df9b689817c
sandraherrerogomez <sandra.herrero93@hotmail.com>
2020-03-22 10:59:03 +0100

    userform

--- js/userform.js
+++ js/userform.js
@@ -8,3 +8,19 @@ var salaryCity2=$("#salaryCity2")[0].value;
 window.location.replace("returnedinfo.html?city1="+city1+"&city2="+city2+"&salary1="+salaryCity1+"&salary2="+salaryCity2);
 
 });
+
+
+
+
+let datos;
+$.get("http://localhost:8080/cities", function(data, status){
+    console.log(data);
+     datos = JSON.parse(data);
+});
+
+$("#currentCity").autocomplete({
+    lookup: datos,
+    onSelect: function (suggestion) {
+        alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+    }
+});
