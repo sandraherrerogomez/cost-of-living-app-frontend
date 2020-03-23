ae00e8629bbd1333fec3a4d550b2fcfa3fc9550a
sandraherrerogomez <sandra.herrero93@hotmail.com>
2020-03-23 12:49:12 +0100

    update

--- js/userform.js
+++ js/userform.js
@@ -12,15 +12,19 @@ window.location.replace("returnedinfo.html?city1="+city1+"&city2="+city2+"&salar
 
 
 
-let datos;
-$.get("http://localhost:8080/cities", function(data, status){
-    console.log(data);
-     datos = JSON.parse(data);
-});
+jQuery.ajax ({
+    url: 'http://localhost:8080/cities',
+    type: "GET",
 
-$("#currentCity").autocomplete({
-    lookup: datos,
-    onSelect: function (suggestion) {
-        alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+
+    success: function(res){
+        var datos = JSON.parse(res);
+        $('#currentCity').autocomplete({
+            lookup: datos,
+            onSelect: function (suggestion) {
+                $('#currentCity').html(suggestion.value);
+            }
+        });
     }
 });
+
