f2e6e301d50f7f7880e24deffd86962b44197dba
mirandm <marcos.miranda.beltran@autodesk.com>
2020-04-05 15:58:02 +0200

    fix bug

--- js/userform.js
+++ js/userform.js
@@ -5,11 +5,18 @@ var salaryCity1=$("#salaryCity1")[0].value;
 var city2=$("#destinationCity")[0].value;
 var salaryCity2=$("#salaryCity2")[0].value;
 
+    if(city1 === "" || city1 === null || city2 === "" || city2 === null || salaryCity1 === "" || salaryCity1 === null ||  salaryCity2 === "" || salaryCity2 === null){
+        alert("The form its not filled");
+        return;
+    }
+
+
     if(city1===city2){
         alert("The value cannot be duplicate, please enter a different city to compare");
     }
+    else{
 window.location.replace("returnedinfo.html?city1="+city1+"&city2="+city2+"&salary1="+salaryCity1+"&salary2="+salaryCity2);
-
+    }
 });
 
 
